e0a682a2ea827998e87ce55487e59dee1142fd5a
Tamara <tamara.elheet018@gmail.com>
2023-12-03 17:17:47 +0300

    User2 added a new feature, feature2

---
+++ Note.java
@@ -0,0 +1,31 @@
+// Note.java
+public class Note {
+    private String title;
+    private String content;
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // New method to edit the content of the note
+    public void editNoteContent(String newContent) {
+        this.content = newContent;
+    }
+}
